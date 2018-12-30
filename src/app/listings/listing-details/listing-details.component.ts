2a1a057d44fde3d5ca0e763a8461b9c0a70a6229
ilia24 <iliademertchian24@gmail.com>
2018-12-30 13:20:06 -0500

    Finaliaze details component

--- src/app/listings/listing-details/listing-details.component.ts
+++ src/app/listings/listing-details/listing-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Listing} from '../listing.model'
+import {ListingService} from '../listing.service'
 
 @Component({
   selector: 'listing-details-modal',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listing-details.component.scss']
 })
 export class ListingDetailsComponent implements OnInit {
+  constructor(private listingService: ListingService){}
+  modalOpen: boolean = false
+  listing: Listing;
 
-  constructor() { }
 
+  // this lets us completely encapsulate the open/close logic within the
+  // component/service relationship
   ngOnInit() {
+    this.listingService.listingSelected.subscribe(
+      (selectedListing: Listing) => {
+        this.listing = selectedListing
+        this.modalOpen = true
+      }
+    )
+  }
+
+  closeListing(){
+    this.modalOpen = false
   }
 
 }
