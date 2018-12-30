5b022cb1636a0b8cd5845a140e88b401327b3071
ilia24 <iliademertchian24@gmail.com>
2018-12-30 13:09:52 -0500

    Initial bootstrap, forms, and setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListingsComponent } from './listings/listings.component';
@@ -18,7 +19,8 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
